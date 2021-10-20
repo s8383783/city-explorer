567d0442c04478e19aa6d9340d61e7c4d7a826ee
Shania Kimbrough <shania_nicolebb@yahoo.com>
2021-10-19 22:53:12 -0400

    server connect

--- src/Movies.js
+++ src/Movies.js
@@ -11,13 +11,20 @@ class Movies extends React.Component {
                 <>
                     {this.props.xMovie.map((movie, idx) => {
                         return (
-                            <Card key= {idx} style={{ width: '18rem' }}>
-                                {/* <Card.Img variant="top" src={movie.image} /> */}
+                            <Card key= {idx} style={{ width: '18rem' }}>                                
                                 <Card.Body>
                                     <Card.Title>{movie.title}</Card.Title>
                                     <Card.Text>
-                                        {movie.overview}
+
+                                        Overview : {movie.overview}
+
+                                        Votes: {movie.vote_count}
+
+                                       Popularity: {movie.popularity}
+
+                                       Release date: {movie.released_on}
                                     </Card.Text>
+                                
                                 </Card.Body>
                             </Card>
                             )
